19f63a1a8950000af49ab4195e3f8cececea3cbf
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-05-06 14:13:19 +0700

    add splash screen

---
+++ app/src/main/java/com/dicoding/moviecatalogmade/activity/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.dicoding.moviecatalogmade.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.dicoding.moviecatalogmade.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
